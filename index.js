1ec243f189c24cd703f6aae2ab0185eceb8b201b
Soldy <4786022+Soldy@users.noreply.github.com>
2020-10-24 22:14:47 +0100

    refactoring
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -3,12 +3,35 @@ const ic = new (require('interactiveConsole')).console();
 
 
 const errorBase = function(){
+
+    let cross = '┣━ ';
+    let last  = '┗━ ';
+
     /*
      * @param {object}
      * @private
      */
-    let debug = function(debugIn){
-        ic.printLn('====');
+    let formaterOne = function(input. separator){
+        return (
+                input[0]
+                    .replace('   at ', separator)
+                    .replace(process.cwd()+'/', ' ')+' | '+
+                ic.style(
+                    parseInt(input[1]).toString(),
+                    {
+                        color : 'cyan'
+                    }
+                )+':'+
+                ic.style(
+                    parseInt(input[2]).toString(),
+                    {
+                        color : 'cyan'
+                    }
+                )+' ) \n'
+        );
+    }
+    let formater = function(debugIn){
+        let out = '';
         let lines = debugIn.stack.split('\n');
         let first = lines[0].split(':');
         ic.printLn(
@@ -20,50 +43,23 @@ const errorBase = function(){
             )+' : '+
             first[1]
         );
-        let tree = '┣━ ';
         if (setup.get('debugPrint') === 'short'){
-            let pieces = lines[1].split(':');
-            tree = '┗━ ';
-            ic.printLn(
-                pieces[0]
-                    .replace('   at ', tree)
-                    .replace(process.cwd()+'/', ' ')+' | '+
-                ic.style(
-                    parseInt(pieces[1]).toString(),
-                    {
-                        color : 'cyan'
-                    }
-                )+':'+
-                ic.style(
-                    parseInt(pieces[2]).toString(),
-                    {
-                        color : 'cyan'
-                    }
-                )+' )'
+            out += formaterOne(
+                lines[1].split(':')
+                last
             );
-        }else 
-            for(let i = 1; lines.length > i ; i++){
-                let pieces = lines[i].split(':');
+        }else
+            for(let i = 1; lines.length > i ; i++)
                 if(i === lines.length-1)
-                    tree = '┗━ ';
-                ic.printLn(
-                    pieces[0]
-                        .replace('   at ', tree)
-                        .replace(process.cwd()+'/', ' ')+' | '+
-                    ic.style(
-                        parseInt(pieces[1]).toString(),
-                        {
-                            color : 'cyan'
-                        }
-                    )+':'+
-                    ic.style(
-                        parseInt(pieces[2]).toString(),
-                        {
-                            color : 'cyan'
-                        }
-                    )+' )'
-                );
-            }
+                    out += formaterOne(
+                        lines[i].split(':')
+                        last
+                    );
+                else
+                    formaterOne(
+                        lines[i].split(':')
+                        cross
+                    );
     };
 };
 
